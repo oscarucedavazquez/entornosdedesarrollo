8be6a4e9035f722644e7d69e7d77490d6d534006
oscar <o.uceda.vazquez@iescristobaldemonroy.es>
2025-02-14 10:15:10 +0100

    nuevos cambios
    14/02/2025 - v.0.0.0 - ouvazquez - Se ha añadido el comentario descriptivo

--- entornosdedesarrollo/src/entornosdedesarrollo/holaMundo.java
+++ entornosdedesarrollo/src/entornosdedesarrollo/holaMundo.java
@@ -5,6 +5,7 @@ public class holaMundo {
 	public static void main(String[] args) {
 		System.out.println("hola mundo");
 		System.out.println("hola mundo, version 2");
+		//los cambios han sido el segundo syso
 	}
 
 }
